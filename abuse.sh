1f6afe12c9966f56294311a86a8e2d151cc0af14
João Zietolie Ciconet <joaopzc@hotmail.com>
2019-07-04 15:30:42 -0300

    Update abuse.sh

--- abuse.sh
+++ abuse.sh
@@ -5,6 +5,6 @@ ips=(`cat ips.txt`)
 for i in "${ips[@]}"
 do
 echo "$i:"
-curl -G https://api.abuseipdb.com/api/v2/check  --data-urlencode "ipAddress=$i" -d maxAgeInDays=90  -H "Key: 2ddb78ae4a31597c9a8e08df91de110364525dd0e7c5f950007d490c21c7df6b34e0e077111c9559" -H "Accept: application/json"
+curl -G https://api.abuseipdb.com/api/v2/check  --data-urlencode "ipAddress=$i" -d maxAgeInDays=90  -H "Key: ADD your API Key here" -H "Accept: application/json"
 echo -e "\n--------------------------------------------------------------------------\n"
 done
